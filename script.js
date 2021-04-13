3c3726153b2ebe164a815f284e3ed9d7059d5730
Aolica <71553069+Aolica@users.noreply.github.com>
2021-04-13 18:56:42 +0900

    commit 2

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function load() {
   alert("load event has been occured")
 }
+document.addEventListener("DOMContentLoaded", function() {
+  function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'ボタンが押されました!';
+    document.body.appendChild(para);
+  }
 
+  const buttons = document.querySelectorAll('button');
+
+  for(let i = 0; i < buttons.length ; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+  }
+});
 window.onload = load;
     
